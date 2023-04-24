3aef98defcdef9f954305760171cb10ef9caa82e
Herman Zhao <hermanzhaozzzz@gmail.com>
2023-04-24 16:17:03 +0800

    git init

---
+++ README.md
@@ -0,0 +1,16 @@
+# 显示所有隐藏文件夹
+
+```
+defaults write com.apple.finder AppleShowAllFiles -boolean true ; killall Finder
+```
+
+# 隐藏所有隐藏文件夹
+
+```
+defaults write com.apple.finder AppleShowAllFiles -boolean false ; killall Finder
+```
+
+```
+conda install spyder-kernels=2.4 matplotlib pandas numpy
+```
+
---
+++ template.py
@@ -0,0 +1,23 @@
+"""Spyder Project: This is a temporary script file.
+
+Created on XXXX-XX-XX
+
+@author: Herman ZHAO
+"""
+
+# %% library
+import os
+
+import numpy as np
+import pandas as pd
+# %% pandas settings
+# pd.set_option("max_colwidth", 250)  # column最大宽度
+# pd.set_option("display.width", 250)  # dataframe宽度
+# pd.set_option("display.max_columns", None)  # column最大显示数
+# pd.set_option("display.maxs_rows", 50)  # row最大显示数
+# %% set directory
+os.chdir("./")
+os.listdir()
+
+# %% start
+
