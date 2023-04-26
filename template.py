ec597374553a1ed45fd0955c9b4c3f690f5a6cf9
Herman Zhao <hermanzhaozzzz@gmail.com>
2023-04-26 14:40:18 +0800

    git init

--- template.py
+++ template.py
@@ -10,11 +10,11 @@ import os
 
 import numpy as np
 import pandas as pd
-# %% pandas settings
-# pd.set_option("max_colwidth", 250)  # column最大宽度
-# pd.set_option("display.width", 250)  # dataframe宽度
-# pd.set_option("display.max_columns", None)  # column最大显示数
-# pd.set_option("display.maxs_rows", 50)  # row最大显示数
+# %%% pandas settings
+# pd.set_option("max_colwidth", 200)  # column最大宽度
+pd.set_option("display.width", 120)  # dataframe宽度
+pd.set_option("display.max_columns", None)  # column最大显示数
+pd.set_option("display.max_rows", 50)  # row最大显示数
 # %% set directory
 os.chdir("./")
 os.listdir()
